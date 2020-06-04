227bafd3c298ccddc5c72fd8640cd514c093de2f
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-06-04 13:15:23 +0100

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -1,30 +1,10 @@
 from tornado.web import url
 from controllers import *
 route = [
-		url(
-			r'/games',
-			handlers.GameCRUDRequestHandler
-		),
-		url(
-			r'/games'+r'/(.*)',
-			handlers.GameCRUDRequestHandler
-		),
-		url(
-			r'/signup',
-			handlers.SignupRequestHandler
-		),
-		url(
-			r'/login',
-			handlers.LoginRequestHandler
-		),
-		url(
-			r'/logout',
-			handlers.LogoutRequestHandler	
-		),
-		url(
-			r'/game/download'+r'/(.*)',
-			handlers.DownloadGameRequestHandler
-		)
-
+url(r'/signup',handlers.SignupRequestHandler),
+url(r'/login',handlers.LoginRequestHandler),
+url(r'/logout',handlers.LogoutRequestHandler),
+url(r'/games',handlers.GameCRUDRequestHandler),
+url(r'/game/download'+r'/(.*)',handlers.DownloadGameRequestHandler),
+url(r'/user',handlers.UserRequestHnadler)
 ]
-				
